87340d9da292eed52ab1d661e768f07636c68404
pensadotech <armando@pensadotech.com>
2018-12-17 18:40:13 -0800

    Project built

---
+++ client/build/precache-manifest.6cf2035be957cc4f1f9d5aab6ef65200.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2b78c30c2ccbe0b0823a",
+    "url": "/static/js/main.2b78c30c.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "2b78c30c2ccbe0b0823a",
+    "url": "/static/css/main.90304208.chunk.css"
+  },
+  {
+    "revision": "8adcd747c0ee9784f48c2f5fd9064d5b",
+    "url": "/index.html"
+  }
+];
